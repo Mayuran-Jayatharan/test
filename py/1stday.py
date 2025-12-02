951180b03271a4a39651c5884c22716098840d4e
Mayuran-Jayatharan <mayu1344@gmail.com>
2025-12-02 11:38:41 +0800

    first commit

---
+++ py/1stday.py
@@ -0,0 +1,38 @@
+# name = "Bob"   #String 
+# age = 10       #Integer
+# weight = 20.5  #Float
+# is_female = False  #Boolean
+
+# print (name)
+# print (type(name))
+
+x = 10
+y = 3
+
+print (x + y)  #Addition
+print (x - y)  #Subtraction
+print (x * y)  #Multiplication
+print (x / y)  #Division
+print (x // y) #Floor Division
+print (x % y)  #Modulus
+print (x ** y) #Exponentiation
+
+# Uni = "Monash" #String
+# Temp = 40      #Integer
+# BMI = 22.5     #Float
+# is_student = True  #Boolean
+
+# print (Uni)
+# print (type (Uni))
+# print (Temp)
+# print (type (Temp))
+# print (BMI)
+# print (type (BMI))
+# print (is_student)
+# print (type (is_student))
+
+
+
+# C = 25 #Celsius temperature
+# F = (C * 9/5) + 32  #Fahrenheit temperature
+# print (F)
