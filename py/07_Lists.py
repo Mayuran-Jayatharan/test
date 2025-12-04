151c2df8c03de244474dc01101e97c8d8669bd47
Mayuran-Jayatharan <mayu1344@gmail.com>
2025-12-04 10:06:29 +0800

    bob the builder

--- py/07_Lists.py
+++ py/07_Lists.py
@@ -12,3 +12,19 @@ print(fruits[-1])   # "orange"
 print(numbers[1:4]) # [2, 3, 4]
 print(numbers[ :3]) # [1, 2, 3]
 print(numbers[2: ]) # [3, 4, 5]
+
+# CRUD a list
+fruits.append("grape")    # Add to end
+fruits.insert(1, "kiwi")  # Insert at index
+fruits.remove("banana")   # Remove by value 
+popped= fruits.pop()      # Remove and return last 
+fruits.sort()             # Sort in place
+fruits.reverse()          # Reverse in place
+
+# List operations
+len(fruits)               # Length
+"apple" in fruits         # Check membership
+fruits + ["mango"]        # Concatenation
+fruits * 2                # Repetition 
+
+print(len(fruits))
